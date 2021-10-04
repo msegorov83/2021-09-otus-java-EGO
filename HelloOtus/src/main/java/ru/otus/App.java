6cad2924c96c3128c8808ff1baac74fc3d0632cb
msegorov <msegorov@stalcom.com>
2021-10-04 16:50:29 +0300

    Homework 01

---
+++ HelloOtus/src/main/java/ru/otus/App.java
@@ -0,0 +1,18 @@
+package ru.otus;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class App {
+    public static void main(String... args) {
+
+        List<String> teachers = Lists.newArrayList("Alexander Orudzhev",
+                "Vitaly Kutsenko",
+                "Vyacheslav Lapin",
+                "Sergey Petrelevich",
+                "Strekalov Pavel");
+
+        System.out.println("Hello teachers: " + Lists.reverse(teachers));
+    }
+}
