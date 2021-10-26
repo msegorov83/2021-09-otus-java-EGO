96e561974805fbd25b7a61f52b18254e5307ca64
msegorov <msegorov@stalcom.com>
2021-10-26 15:02:50 +0300

    fix: Homework02

--- hw02-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -6,27 +6,21 @@ public class CustomerReverseOrder {
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-
-    LinkedHashMap<Long,Customer>map = new LinkedHashMap<>();
-    Map<Long,Customer> reverseOrderedMap = new LinkedHashMap<>();
+    LinkedHashSet<Customer> set = new LinkedHashSet <>();
+    int currentInd = 0;
 
     public void add(Customer customer) {
-        map.put(customer.getId(), customer);
-        reverseOrderedMap=reverseMap(map);
+        set.add(customer);
     }
 
     public Customer take() {
-        Customer currCustomer = reverseOrderedMap.entrySet().stream().findFirst().get().getValue();
-        reverseOrderedMap.remove(currCustomer.getId());
+
+        List<Customer> list = new ArrayList<>(set);
+        Collections.reverse(list);
+        Customer currCustomer = list.get(currentInd);
+        currentInd++;
+
         return currCustomer;
     }
 
-    private static <K, V> LinkedHashMap<K, V> reverseMap(LinkedHashMap<K, V> map)
-    {
-        LinkedHashMap<K, V> reversedMap = new LinkedHashMap<>();
-        List<K> reverseOrderedKeys = new ArrayList<>(map.keySet());
-        Collections.reverse(reverseOrderedKeys);
-        reverseOrderedKeys.forEach((key)->reversedMap.put(key,map.get(key)));
-        return reversedMap;
-    }
 }
--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -9,24 +9,17 @@ public class CustomerService {
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
-    Map<Customer, String> map = new HashMap<>();
-
+      SortedMap<Customer, String> sortedMap = new TreeMap<>(Comparator.comparing(customer -> customer.getScores()));
 
     public Map.Entry<Customer, String> getSmallest() {
 
-        Map<Customer,String>sorted = new LinkedHashMap<>();
-        Stream<Map.Entry<Customer,String>> st = map.entrySet().stream();
-
-        st.sorted(Comparator.comparing(e -> e.getKey().getScores()))
-                .forEach(e ->sorted.put(e.getKey(),e.getValue()));
-
-        return  sorted.entrySet().stream().findFirst().get();
+        return sortedMap.entrySet().stream().findFirst().get();
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
 
         TreeMap<Long, Map.Entry<Customer, String> > treeMap = new TreeMap<>();
-        for (Map.Entry<Customer, String> e: map.entrySet()) {
+        for (Map.Entry<Customer, String> e: sortedMap.entrySet()) {
             treeMap.put(e.getKey().getScores(),e);
         }
 
@@ -39,6 +32,6 @@ public class CustomerService {
 
     public void add(Customer customer, String data) {
         Customer buff = new Customer(customer);
-        map.put(buff, data);
+        sortedMap.put(buff, data);
     }
 }
