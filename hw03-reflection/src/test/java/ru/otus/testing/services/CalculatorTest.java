dec6fd651292683a091a4da7d93d8e9a275e41ac
msegorov <msegorov@stalcom.com>
2021-11-02 11:46:20 +0300

    fix: Homework 03

--- hw03-reflection/src/test/java/ru/otus/testing/services/CalculatorTest.java
+++ hw03-reflection/src/test/java/ru/otus/testing/services/CalculatorTest.java
@@ -12,26 +12,28 @@ class CalculatorTest {
 
     @BeforeEach
     public void setUp(){
-        System.out.println("Message before test");
         calculator = new Calculator();
     }
 
     @Test
     public void testMultiply() {
-        System.out.println("-- testMultiply --");
         assertEquals(20, calculator.multiply(4, 5));
     }
 
     @Test
     void testMultiplyWithZero() {
-        System.out.println("-- testMultiplyWithZero --");
         assertEquals(0, calculator.multiply(0, 5));
         assertEquals(0, calculator.multiply(5, 0));
     }
 
+    @Test
+    void testDivideByZero() {
+        assertEquals(0, calculator.divide(1, 0));
+    }
+
     @AfterEach
     public void clear(){
-        System.out.println("Message after test");
         calculator = null;
+
     }
 }
--- hw03-reflection/src/test/java/ru/otus/testing/services/RunnerTests.java
+++ hw03-reflection/src/test/java/ru/otus/testing/services/RunnerTests.java
@@ -6,6 +6,7 @@ import ru.otus.testing.annotation.Test;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class RunnerTests {
 
@@ -13,22 +14,24 @@ public class RunnerTests {
 
         Class<CalculatorTest> clazz = CalculatorTest.class;
         Constructor[] constructors = clazz.getDeclaredConstructors();
+        Constructor constructor =
+                Arrays.stream(constructors).filter(e -> e.getName() == clazz.getName()).findAny().get();
 
-        Constructor constructor = null;
-        for (int i = 0; i < constructors.length; i++) {
-            constructor = constructors[i];
+        for (Method method : clazz.getDeclaredMethods()) {
             Object obj = constructor.newInstance();
+            if (method.isAnnotationPresent(Test.class)) {
+                executeBefore(obj);
 
-            Method[] methods = obj.getClass().getDeclaredMethods();
-            for (Method method : methods) {
-                if (method.isAnnotationPresent(Test.class)) {
-                    executeBefore(obj);
+                try {
                     method.invoke(obj);
-                    executeAfter(obj);
+                    System.out.println("TEST: " + method.getName() + " SUCCESS");
+                } catch (InvocationTargetException e) {
+                    System.out.println("TEST: " + method.getName() + " FAILED");
                 }
+
+                executeAfter(obj);
             }
         }
-
     }
 
     private static void executeBefore(Object obj) throws InvocationTargetException, IllegalAccessException {
@@ -52,5 +55,4 @@ public class RunnerTests {
         }
 
     }
-
 }
