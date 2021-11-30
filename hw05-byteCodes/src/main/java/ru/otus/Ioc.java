b1e963fe1c0dc66ed06c163e4974e91e04886759
msegorov <msegorov@stalcom.com>
2021-11-30 10:00:08 +0300

    fix: homework05

--- hw05-byteCodes/src/main/java/ru/otus/Ioc.java
+++ hw05-byteCodes/src/main/java/ru/otus/Ioc.java
@@ -1,8 +1,9 @@
 package ru.otus;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.*;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +14,6 @@ public class Ioc {
     }
 
     static TestLoggingInterface createTestLoging() {
-
        InvocationHandler handler = new TestLoggingInvocationHandler(new TestLogging());
         TestLoggingInterface testLoggingInterface = (TestLoggingInterface) Proxy.newProxyInstance(Ioc.class.getClassLoader(),
                 new Class<?>[]{TestLoggingInterface.class}, handler);
@@ -28,25 +28,37 @@ public class Ioc {
             this.target = target;
         }
 
-         public Object getTestLogging() {
+        public Object getTestLogging() {
              return target;
          }
 
         @Override
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-
-            methods.merge(method.getName(), 1 ,Integer::sum);
-            Class clazz = getTestLogging().getClass();
+        public Object invoke(Object proxy, Method method, Object[] args) throws  IOException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+            var loader = new TestClassLoader();
+            Class<?> clazz = loader.defineClass("ru.otus.TestLogging");
             Method[] methods = clazz.getDeclaredMethods();
             for (Method m : methods) {
-
-                if (m.isAnnotationPresent(Log.class) & m.getName().equals(method.getName()) & m.getParameterCount()==args.length) {
-                        System.out.println("executed method: " + method.getName() + " param: " + Arrays.stream(args).toList());
-                        return method.invoke(target, args);
+                if (m.getName().equals(method.getName()) & m.getParameterCount()==args.length & m.isAnnotationPresent(Log.class)) {
+                    System.out.println("executed method: " + method.getName() + " param: " + Arrays.stream(args).toList());
+                    m.setAccessible(true);
+                    Constructor constructor = clazz.getDeclaredConstructor();
+                    constructor.setAccessible(true);
+                    return m.invoke(constructor.newInstance(),args);
                 }
             }
-
             return null;
+        }
+
+        private static class TestClassLoader extends ClassLoader {
+            Class<?> defineClass(String className) throws IOException {
+                var file = new File(getFileName(className));
+                byte[] bytecode = Files.readAllBytes(file.toPath());
+                return super.defineClass(className, bytecode, 0, bytecode.length);
+            }
+
+            String getFileName(String className) {
+                return "clazz" + File.separator + className.substring(className.lastIndexOf('.') + 1) + ".class";
+            }
         }
 
         @Override
