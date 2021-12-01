c4ddd68c6d822c32027f82252a494d3be241136f
msegorov <msegorov@stalcom.com>
2021-12-01 10:19:37 +0300

    fix: Homework05. added method getInfoAboutClazz

--- hw05-byteCodes/src/main/java/ru/otus/Ioc.java
+++ hw05-byteCodes/src/main/java/ru/otus/Ioc.java
@@ -4,46 +4,54 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.*;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Ioc {
+    private static ArrayList<Method> loggingMethods = new ArrayList();
 
     private Ioc() {
     }
 
-    static TestLoggingInterface createTestLoging() {
-       InvocationHandler handler = new TestLoggingInvocationHandler(new TestLogging());
+    static TestLoggingInterface createTestLoging()  {
+        TestLogging testLogging = new TestLogging();
+        getInfoAboutClazz(testLogging);
+        InvocationHandler handler = new TestLoggingInvocationHandler(testLogging, loggingMethods);
         TestLoggingInterface testLoggingInterface = (TestLoggingInterface) Proxy.newProxyInstance(Ioc.class.getClassLoader(),
                 new Class<?>[]{TestLoggingInterface.class}, handler);
         return testLoggingInterface;
     }
 
+    static private void getInfoAboutClazz(Object target)  {
+        try {
+            var loader = new TestLoggingInvocationHandler.TestClassLoader();
+            Class<?> clazz = loader.defineClass(target.getClass().getName());
+            Method[] declaredMethods = clazz.getDeclaredMethods();
+            for (Method m : declaredMethods) {
+                if (m.isAnnotationPresent(Log.class)) {
+                    loggingMethods.add(m);
+                }
+            }
+        } catch (IOException e) {}
+
+    }
+
     static class TestLoggingInvocationHandler implements InvocationHandler {
         private final Object target;
-        private final Map<String, Integer> methods = new HashMap<>();
+        private final ArrayList<Method> loggingMethod;
 
-        TestLoggingInvocationHandler(Object target) {
+        TestLoggingInvocationHandler(Object target, ArrayList<Method> loggingMethod) {
             this.target = target;
+            this.loggingMethod = loggingMethod;
         }
 
-        public Object getTestLogging() {
-             return target;
-         }
-
         @Override
-        public Object invoke(Object proxy, Method method, Object[] args) throws  IOException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-            var loader = new TestClassLoader();
-            Class<?> clazz = loader.defineClass("ru.otus.TestLogging");
-            Method[] methods = clazz.getDeclaredMethods();
-            for (Method m : methods) {
-                if (m.getName().equals(method.getName()) & m.getParameterCount()==args.length & m.isAnnotationPresent(Log.class)) {
-                    System.out.println("executed method: " + method.getName() + " param: " + Arrays.stream(args).toList());
-                    m.setAccessible(true);
-                    Constructor constructor = clazz.getDeclaredConstructor();
-                    constructor.setAccessible(true);
-                    return m.invoke(constructor.newInstance(),args);
+        public Object invoke(Object proxy, Method method, Object[] args) throws  InvocationTargetException, IllegalAccessException {
+
+            for (Method m : loggingMethod) {
+                if (m.getName().equals(method.getName()) & m.getParameterCount()==args.length ) {
+                  System.out.println("executed method: " + method.getName() + " param: " + Arrays.stream(args).toList());
+                  return method.invoke(target, args);
                 }
             }
             return null;
